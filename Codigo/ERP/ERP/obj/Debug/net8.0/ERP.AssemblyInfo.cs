613834cee44af0987f73ba98428d9e5c89b2dde4
Dylan Rodríguez <dylrodriguez@estudiantec.cr>
2024-09-21 11:52:10 -0600

    Pequeños cambios al sidebar

--- Codigo/ERP/ERP/obj/Debug/net8.0/ERP.AssemblyInfo.cs
+++ Codigo/ERP/ERP/obj/Debug/net8.0/ERP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70d7d6a2558009d9476c6b2647f764ae1fe4decc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc9d7a5da7b0808898dbb94e1891bf01ee26d3c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
