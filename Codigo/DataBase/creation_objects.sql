0516938013a87227a13072b004f2054c6b252f45
SERAFINO <ymontenegro@estudiantec.cr>
2024-09-22 20:58:17 -0600

    Actualizacion de version.

--- Codigo/DataBase/creation_objects.sql
+++ Codigo/DataBase/creation_objects.sql
@@ -1,7 +1,7 @@
 /*
 Name   : ERP
 Link   : https://github.com/DylanRodriguez22/Proyecto1_Bases1.git
-Version: 20/09/2024
+Version: 22/09/2024
 Autores: Yosimar Montenegro y Dylan Rodríguez
 --------------------------------------------------------------------
 */
@@ -274,43 +274,3 @@ CREATE TABLE Ventas.TipoCedula ( -- Tabla catalogo_tipoCedula
 	ID INT IDENTITY (1, 1) PRIMARY KEY,
 	tipo VARCHAR (50) NOT NULL
 );
-
-/*
-Inserts en las tablas catalogo no modificables.
-
-Funcionalidad: Cada tabla posee una PK que la identifica (en su mayoría ID Integers), lo cual permite hacer agrupamientos rapidos
-			   Por cada tabla puede consultarse de forma generar para llenar ComboBoxes necesarios, Pero no pueden modificarse, unicamente Consultas, Deletes E Inserts.
-*/
-
-USE ERP
-GO
-
-INSERT INTO Ventas.TipoCedula (tipo)
-VALUES
-('Cédula Física'),
-('Cédula Jurídica');
-
-INSERT INTO RRHH.Departamento (nombre, codigo)
-VALUES 
-('Recursos Humanos', 'D1'),
-('Produccion', 'D2'),
-('Ventas', 'D3');
-
-INSERT INTO Ventas.Genero (descripcion)
-VALUES 
-('Masculino'),
-('Femenino'),
-('Otro');
-
-INSERT INTO Ventas.Estado (descripcion)
-VALUES
-('Aceptada'),
-('Rechazada'),
-('En proceso'),
-('Anulada');
-
-INSERT INTO Ventas.Etapa (descripcion)
-VALUES
-('Pendiente'),
-('En proceso'),
-('Realizada')
