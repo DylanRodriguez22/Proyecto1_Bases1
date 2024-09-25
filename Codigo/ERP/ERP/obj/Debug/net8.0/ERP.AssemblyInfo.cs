050fa2e0e62c464d14125a22d38dc8da88cf90c3
Dylan Rodríguez <dylrodriguez@estudiantec.cr>
2024-09-25 10:23:31 -0600

    Commit por si acaso

--- Codigo/ERP/ERP/obj/Debug/net8.0/ERP.AssemblyInfo.cs
+++ Codigo/ERP/ERP/obj/Debug/net8.0/ERP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b457a7ee6f044b8d86c176e752af860d5e915eb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd403721ab406b941672182e8107c1ce257043d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
